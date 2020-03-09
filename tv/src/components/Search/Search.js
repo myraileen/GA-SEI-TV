96c510bc443a6bf62befe70113bba13cfb3566dd
Myra Fah <FahMyra@JohnDeere.com>
2020-03-09 09:04:47 -0500

    updated styling... added input search field

--- tv/src/components/Search/Search.js
+++ tv/src/components/Search/Search.js
@@ -14,25 +14,26 @@ class Search extends Component {
     return (
       <>
         <form className='search'>Search
+        <div>
             <label>Trending</label>
             <input name='tv'
               type='radio'
               value='Trending'
               onChange={this.handleOptionChange}
-            />
+            /></div><div>
             <label> Popular</label>
             <input name='tv'
               type='radio'
               value='Popular'
               onChange={this.handleOptionChange}
-            />
-            <label>Genre</label>
+            /></div>
+            {/* <label>Genre</label>
             <input name='tv'
               type='radio'
               value='Genre'
               onChange={this.handleOptionChange}
-            />
-            <input type='text' value={searchText} ></input>
+            /> */}<label>Search Shows:
+            <input type='text' value={this.props.searchText} /></label>
         </form>
       </>
     );
